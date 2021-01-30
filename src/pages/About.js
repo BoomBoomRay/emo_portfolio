a5b0923382cf94e143a0e7e9e5df8e3fd3be0977
Ray Romero <chinoromero213@gmail.com>
2021-01-30 10:45:17 -0800

    about setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Navbar, Footer, Wrapper } from '../components';
 
 export default function About() {
   return (
     <Wrapper>
       <Navbar />
-      <h1>ABout</h1>
+      <Container>
+        <div className='about-div-container'>
+          <div>first blog of writing</div>
+          <div>secondj blog of writing</div>
+        </div>
+        <div className='about-div-container'>
+          <div>first blog of writing</div>
+          <div>secondj blog of writing</div>
+        </div>
+      </Container>
       <Footer />
     </Wrapper>
   );
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1em;
+  padding: 20px;
+  /* height: 100vh; */
+  .about-div-container {
+    border: 1px solid red;
+    width: 100%;
+  }
+  @media (min-width: 500px) {
+    width: 100%;
+    max-width: 700px;
+  }
+  div {
+  }
+`;
