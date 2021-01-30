43c656057fd8a7bc0e463eb26ffa52b7febb0bfa
Ray Romero <chinoromero213@gmail.com>
2021-01-30 12:22:50 -0800

    About compnt complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Navbar, Footer, Wrapper } from '../components';
+import { TiSocialInstagram } from 'react-icons/ti';
 
 export default function About() {
   return (
@@ -8,12 +9,46 @@ export default function About() {
       <Navbar about='About' />
       <Container>
         <div className='about-div-container'>
-          <div>first blog of writing</div>
-          <div>secondj blog of writing</div>
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.{' '}
+          </p>
+          <h2>Contact Info</h2>
+          <p>
+            Emerson@gmail.com <br />
+            Mobile: 604 122 1222 <br />
+            Social:
+            <span>
+              <a href='https://instagram.com/emmolau' target='_'>
+                <TiSocialInstagram />
+              </a>
+            </span>
+          </p>
         </div>
         <div className='about-div-container'>
-          <div>first blog of writing</div>
-          <div>secondj blog of writing</div>
+          <h2>Publications</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.{' '}
+          </p>
+          <h2>Clients</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.{' '}
+          </p>
         </div>
       </Container>
       <Footer />
@@ -26,15 +61,34 @@ const Container = styled.div`
   grid-template-columns: 1fr 1fr;
   gap: 1em;
   padding: 20px;
-  /* height: 100vh; */
+  height: 100vh;
   .about-div-container {
-    border: 1px solid red;
     width: 100%;
+  }
+  .about-div-container {
+    text-transform: uppercase;
+    font-family: var(--ff-secondary);
+  }
+  .about-div-container p {
+    line-height: 2rem;
+    margin-bottom: 30px;
+  }
+  .about-div-container h2 {
+    margin-bottom: 15px;
+  }
+  .about-div-container span {
+    margin-left: 20px;
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .about-div-container span a {
+    color: black;
+  }
+  .about-div-container span a:hover {
+    color: red;
   }
   @media (min-width: 500px) {
-    width: 100%;
     max-width: 700px;
-  }
-  div {
+    margin: auto;
   }
 `;
