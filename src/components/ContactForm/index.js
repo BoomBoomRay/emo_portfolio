c974550d587005c57f4b04aec377f740258e64df
Ray Romero <chinoromero213@gmail.com>
2021-01-31 23:37:44 -0800

    email back end connect/need to fix errros

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,25 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Wrapper } from './ContactFormElement';
 
 export default function ContactForm() {
+  const [state, setState] = useState({ from: '', subject: '', text: '' });
+  const [onSuccess, setOnSuccess] = useState({ boolean: false, message: '' });
+  const [errorMessage, setErrorMessage] = useState({
+    from: '',
+    subject: '',
+    text: '',
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const sendContactEmail = (contactInfo) => {
+    axios
+      .post('http://localhost:3000/contact', contactInfo)
+      .then((data) => {
+        setOnSuccess({ boolean: true, message: data.data.message });
+      })
+      .catch((err) => {
+        setErrorMessage({
+          from: err.response.from,
+          subject: err.response.subject,
+          text: err.response.text,
+        });
+      });
+  };
+
+  console.log(errorMessage);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendContactEmail(state);
+    setState({ from: '', subject: '', text: '' });
+  };
+
+  const { from, text, subject } = state;
   return (
     <Wrapper>
       <h1>Contact</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input label='Name' placeholder='Name' />
+          <input
+            onChange={handleChange}
+            name='from'
+            value={from}
+            type='email'
+            label='Email'
+            placeholder='Email'
+          />
         </div>
         <div>
-          <input label='Email' placeholder='Email' />
+          <input
+            onChange={handleChange}
+            name='subject'
+            value={subject}
+            type='text'
+            label='Subject'
+            placeholder='Subject'
+          />
         </div>
         <div>
-          <input label='Subject' placeholder='Subject' />
+          <textarea
+            onChange={handleChange}
+            value={text}
+            name='text'
+            type='text'
+            label='Message'
+            placeholder='Message'
+          />
         </div>
         <div>
-          <textarea label='Message' placeholder='Message' />
-        </div>
-        <div>
-          <input value='Send' type='button' />
+          {onSuccess.boolean ? (
+            <h1 style={{ marginTop: '30px' }}>{onSuccess.message}</h1>
+          ) : (
+            <input onClick={handleSubmit} value='Send' type='button' />
+          )}
         </div>
       </form>
     </Wrapper>
